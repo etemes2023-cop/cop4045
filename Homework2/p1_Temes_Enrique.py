31798d1815a7bbef7d72a58abb6dbb285bcebc52
etemes2023 <etemes2023@fau.edu>
2026-09-15 10:50:34 -0400

    Finished problem 1 by creating an implementation of the class Parse_Files, which contains all code. All output works as intended.

--- Homework2/p1_Temes_Enrique.py
+++ Homework2/p1_Temes_Enrique.py
@@ -1,3 +1,4 @@
+import ast
 class ParseFiles:
     def __init__(self):
         pass
@@ -22,6 +23,38 @@ class ParseFiles:
             print("File ", inp, "doesn't exist.")
             raise
 
+    def parse_functions(self, filename: str) -> tuple:
+        """Reads input file and creates a tuple of tuples. Each individual tuple contains the function definition line number,
+        the function name, each function argument, and a list of code for that function definition. Empty lines or comments
+        are skipped."""
+        try:
+            input_file = open(filename, "r")
+            source_code = input_file.readlines()
+            tree = ast.parse("".join(source_code))
+            functions = []
+            for node in ast.walk(tree):
+                if isinstance(node, ast.FunctionDef):
+                    starting_number = node.lineno
+                    name = node.name
+                    arguments = ast.unparse(node.args)
+                    ending_number = node.end_lineno
+                    code = []
+                    for i in range(starting_number, ending_number+1):
+                        if source_code[i-1].strip() == "" or source_code[i-1].strip().startswith("#"): 
+                            continue
+                        code.append(source_code[i-1])
+                    functions.append((starting_number, name, arguments, "".join(code)))
+
+            functions.sort(key=lambda x:x[1])
+            return tuple(functions)  
+           
+        except FileNotFoundError:
+                print("File ", filename, "doesn't exist.")
+                raise
+
     def main(self, inp, out):
         self.line_number(inp, out)
-        
+        print(self.parse_functions(inp))
+
+parse = ParseFiles()       
+parse.main("C:/Users/eteme/Documents/FAU/Python Programming/Homework1/p2_Temes_Enrique.py", "output.txt")
