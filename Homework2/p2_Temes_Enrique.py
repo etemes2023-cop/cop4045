dc8f55687545ad8edcffc2cbc56d24f38c34df80
etemes2023 <etemes2023@fau.edu>
2026-09-15 14:36:15 -0400

    Finished part C of Question 2 by taking a list of people's names and replacing the middle name with the first letter of that initial followed by a period.

--- Homework2/p2_Temes_Enrique.py
+++ Homework2/p2_Temes_Enrique.py
@@ -2,6 +2,7 @@ class Comprehension:
     def __init__(self):
         pass
 
+    #Part A
     def number_com(self):
         valid = []
         for a in range(1, 11):
@@ -15,16 +16,27 @@ class Comprehension:
                             valid.append(numbers)
         return valid
 
+    #Part B
     def string_valid(self, strings):
         valid = []
         for string in strings:
             if len(string) < 5:
                 valid.append((string.lower(), len(string)))
         return valid
+
+    #Part C
+    def revised_names(self, names):
+        valid = []
+        for name in names:
+            name_parts = name.split()
+            name_parts[1] = name_parts[1][0] + "."
+            valid.append(" ".join(name_parts))
+        return valid
     
     def main(self):
-        strings = ["One", "SEVEN", "Three", "Two", "Ten"]
-        print(self.string_valid(strings))
+        names = ['Christopher Ashton Kutcher', 'Elizabeth Stamatina Fey']
+        #strings = ["One", "SEVEN", "Three", "Two", "Ten"]
+        print(self.revised_names(names))
 
 solution = Comprehension()
 solution.main()
