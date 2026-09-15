3f13fe3902c03355a64477562c9259ee38e07a9e
etemes2023 <etemes2023@fau.edu>
2026-09-15 16:15:11 -0400

    Finished part F of Question 2 by taking a string and pair every index where there is vowel with the actual vowel itself as a key/value pair and adding it to a dictionary to be returned and printed.

--- Homework2/p2_Temes_Enrique.py
+++ Homework2/p2_Temes_Enrique.py
@@ -48,14 +48,31 @@ class Comprehension:
         for string in string_list:
             string_lengths[string] = len(string)
         return string_lengths
+
+    #Part F
+    def vowel_count(self, text):
+        vowels = {}
+        index = 0
+        for letter in text:
+            if letter.lower() == 'a' or letter.lower() == 'e' or letter.lower() == 'i' or letter.lower() == 'o' or letter.lower() == 'u':
+                vowels[index] = letter
+            index += 1
+        return vowels
     
     def main(self):
+        text = "Hello world"
         s = ["One", "Two", "Three"]
-        #lst1 = ["Spam", "Trams", "Elbows", "Tops", "Astral"]
-        #lst2 = ["Bowels", "Sample", "Altars", "Stop", "Course", "Smart"]
-        #names = ['Christopher Ashton Kutcher', 'Elizabeth Stamatina Fey']
-        #strings = ["One", "SEVEN", "Three", "Two", "Ten"]
-        print(self.find_string_lengths(s))
+        lst1 = ["Spam", "Trams", "Elbows", "Tops", "Astral"]
+        lst2 = ["Bowels", "Sample", "Altars", "Stop", "Course", "Smart"]
+        names = ['Christopher Ashton Kutcher', 'Elizabeth Stamatina Fey']
+        strings = ["One", "SEVEN", "Three", "Two", "Ten"]
+
+        print(self.number_com())
+        print(f"\n{self.string_valid(strings)}")
+        print(f"\n{self.revised_names(names)}")
+        print(f"\n{self.find_anagrams(lst1, lst2)}")
+        print(f"\n{self.find_string_lengths(s)}")
+        print(f"\n{self.vowel_count(text)}")
 
 solution = Comprehension()
 solution.main()
